f207ec8189b8ad642a2ae892f2b22c6f9d04395a
fakhri1748 <muhfakhri237651@student.smkn1kandeman.sch.id>
2024-10-03 11:15:31 +0700

    kelompok3.php

---
+++ kelompok3.php
@@ -0,0 +1,9 @@
+<html>
+  <head>
+    <title>KELOMPOK 3</title>
+  </head>
+  <body>
+    <h1>Ais Love Budi</h1>
+    <marquee>cieeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee</marquee>
+  </body>
+</html>
